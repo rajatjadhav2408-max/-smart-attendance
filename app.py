9ad1f60e6542f840910e9518ee1caec4505b4987
rajatjadhav2408-max <rajatjadhav2408@gmail.com>
2026-05-23 12:39:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ database_url = os.environ.get('DATABASE_URL', 'sqlite:///attendance.db')
 if database_url.startswith('postgres://'):
     database_url = database_url.replace('postgres://', 'postgresql://', 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///attendance.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
